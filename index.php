37da20548939159b8cbef9b9140b8e8048fdf337
Fuentes <alaviserafin@gmail.com>
2016-09-05 16:53:37 -0400

    like

--- index.php
+++ index.php
@@ -79,7 +79,27 @@
         }
     });
 </script>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1152675844770748',
+            xfbml      : true,
+            version    : 'v2.4'
+        });
 
+        FB.Event.subscribe('edge.create', function(response) {
+            alert("thank you for like ok");
+        });​
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 <script type="text/javascript">
     $(document).ready(function () {
 
