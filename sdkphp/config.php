fcbaf9a894492efeaa426743de4700f2bafdc484
Fuentes <alaviserafin@gmail.com>
2016-09-07 18:03:23 -0400

    psk sdk

---
+++ sdkphp/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alavi
+ * Date: 07/09/2016
+ * Time: 05:30 PM
+ */
+const APP_ID="761790700589965";
+const  APP_SECRET="a8c22633a653c57bf71c8dcf56899c11";
+const  APP_VERSION="v2.5";
+const  APP_URL="conectate.herokuapp.com";
